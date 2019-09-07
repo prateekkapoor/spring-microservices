40fbecedcd517580438ab0422d0a5f57f099a6a6
Prateek Kapoor <prateekapoor1@gmail.com>
2019-09-07 15:25:58 +0530

    Add files via upload

---
+++ spring-cloud-function/spring-cloud-function-aws-demo/src/main/java/com/spring/cloud/function/aws/demo/SpringCloudFunctionAwsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.function.aws.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudFunctionAwsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudFunctionAwsDemoApplication.class, args);
+	}
+
+}
